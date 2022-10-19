7adc3f6c8e090c1a93ac0b9fc660648765cd7108
yi.shi <kukudeshiyi@yeah.net>
2022-10-19 21:12:51 +0800

    feat: reverseList

--- base/algorithm/02_reverseLinkedList.js
+++ base/algorithm/02_reverseLinkedList.js
@@ -1,4 +1,18 @@
 // 反转链表
+function reverseList(head) {
+  if (!head) {
+    return head;
+  }
+  let prevNode = null;
+  let currentNode = head;
+  while (currentNode) {
+    const nextNode = currentNode.next;
+    currentNode.next = prevNode;
+    prevNode = currentNode;
+    currentNode = nextNode;
+  }
+  return prevNode;
+}
 // 链表中环的检测
 // 两个有序的链表合并
 // 删除链表倒数第 k 个节点
