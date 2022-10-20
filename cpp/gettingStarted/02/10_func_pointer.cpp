28e77038c16075e5845c7d681b686d83b98d97a4
liqi.shi <liqi.shi@shopee.com>
2022-10-20 14:21:59 +0800

    feat: add cpp case

--- cpp/gettingStarted/02/10_func_pointer.cpp
+++ cpp/gettingStarted/02/10_func_pointer.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
 using namespace std;
 
+const string* cout_hello(){
+  static string msg = "hello";
+  return &msg;
+}
+const string* cout_hi(){
+  static string msg = "hi";
+  return &msg;
+}
+const string* cout_not_bad(){
+  static string msg = "not bad";
+  return &msg;
+}
+
+void display_message( const string* (*fn)() = 0 ){
+  if( ! fn ){
+    cerr << "fn is not a function" << endl;
+  }
+  const string *pi = fn();
+  cout << "msg is" << ' ' << (*pi) << endl;
+}
+
 int main(){
-  
+  const string* (*fn_array[]) () = {
+    cout_hello,
+    cout_hi,
+    cout_not_bad
+  };
+  for( int ix = 0; ix < 3 ; ++ix ){
+      display_message( fn_array[ix] ); 
+  }
+  enum fn_type {
+    hello,
+    hi,
+    not_bad
+  }
+  display_message( fn_array[ not_bad ] );
   return 0;
 }
