9e12a8dc1f44872c43eadd48ae9bfe574956dc01
yi.shi <kukudeshiyi@yeah.net>
2022-10-21 09:05:41 +0800

    feat: add cpp case

---
+++ cpp/gettingStarted/03/01_pointer_calc.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+// 实现一个与容器类型无关的 find 函数，支持 array 和 vector
+
+// 1、一种是通过第一个数组的地址与数组的 size 来实现元素的遍历
+template <typename elemType>
+elemType* find( const elemType *array, int size, const elemType &value ){
+  if( ! array || size < 0 ){
+    cerr << "find() params error" << endl;
+    return 0;
+  }
+  for( int ix = 0; ix < size; ++ix ){
+    if( array[ix] == value ){
+      return &array[ix];
+    }
+  }
+  return 0;
+}
+
+// 对于第一种还有一种写法
+template <typename elemType>
+elemType* find_pro( const elemType *array, int size, const elemType &value ){
+  if( ! array || size < 0 ){
+    cerr << "find() params error" << endl;
+    return 0;
+  }
+  for( int ix = 0; ix < size; ++ix, ++array ){
+    if( *array == value ){
+      return array;
+    }
+  }
+  return 0;
+}
+
+// 2、一种是通过数组第一个元素与最后一个元素来实现元素的遍历
+template <typename elemType>
+elemType* find( const elemType *first, const elemType *last, const elemType & value ){
+  if( ! first || ! last ){
+    cerr << "find() params error" << endl;
+    return 0;
+  }
+  for( ; first != last; ++first ){
+    if( *first == value ){
+      return first;
+    }
+  }
+  return 0;
+} 
+
+int main(){
+
+  // 如何获取数组的指针
+  // 如此可见与函数一致,函数名即代表内存地址，在这里数组名代表的是第一个元素的地址。
+  int arr[6];
+  arr[0] = 1;
+  cout << arr << ' '<< *arr << endl; 
+
+
+  return 0;
+}
