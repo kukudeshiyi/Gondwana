d15d1d2dc5510706826b39cbb6995ab171ef3402
liqi.shi <liqi.shi@shopee.com>
2022-10-27 13:56:18 +0800

    feat: add js

--- frontend/javascript/04_lodash_get.js
+++ frontend/javascript/04_lodash_get.js
@@ -1,1 +1,22 @@
 // 实现 lodash 的_.get
+
+// object (Object): The object to query.
+// path (Array|string): The path of the property to get.
+// [defaultValue] (*): The value returned for undefined resolved values.
+
+// var object = { 'a': [{ 'b': { 'c': 3 } }] };
+
+// _.get(object, 'a[0].b.c');
+// => 3
+
+// _.get(object, ['a', '0', 'b', 'c']);
+// => 3
+
+// _.get(object, 'a.b.c', 'default');
+// => 'default'
+
+function get(obj, path, defaultValue) {
+  if (typeof obj !== "object") {
+    return defaultValue;
+  }
+}
