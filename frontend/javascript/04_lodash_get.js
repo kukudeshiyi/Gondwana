3475a2cd08f82ae3ef5a7f4477e02054b8d4cd1c
liqi.shi <liqi.shi@shopee.com>
2022-10-27 19:00:52 +0800

    feat:实现 loadash 的 get

--- frontend/javascript/04_lodash_get.js
+++ frontend/javascript/04_lodash_get.js
@@ -15,8 +15,19 @@
 // _.get(object, 'a.b.c', 'default');
 // => 'default'
 
+const REG_EXP = /^([a-zA-Z0-9]{1,})\[([0-9]{1,})\]$/;
+
 function get(obj, path, defaultValue) {
   if (typeof obj !== "object") {
     return defaultValue;
   }
+  const paths = path.replace(/\[(\d+)\]/g, ".$1");
+  let res = obj;
+  for (let path of paths) {
+    res = obj[path];
+    if (res === undefined) {
+      return defaultValue;
+    }
+  }
+  return res;
 }
