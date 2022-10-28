29325bbfb4c5e4099fd38a1a64912515f7fa9b14
yi.shi <kukudeshiyi@yeah.net>
2022-10-28 08:52:35 +0800

    feat:实现循环队列

--- base/algorithm/03_circularQueue.js
+++ base/algorithm/03_circularQueue.js
@@ -1,1 +1,31 @@
 // 循环队列
+class CircularQueue {
+  constructor(length) {
+    this.queue = [];
+    this.length = length;
+    this.head = 0;
+    this.tail = 0;
+  }
+  isFull() {
+    return (this.tail + 1) % this.length === this.head;
+  }
+  isEmpty() {
+    return this.head === this.tail;
+  }
+  enqueue(value) {
+    if (this.isFull()) {
+      return false;
+    }
+    this.queue[this.tail] = value;
+    this.tail = (this.tail + 1) % this.length;
+    return true;
+  }
+  dequeue() {
+    if (this.isEmpty()) {
+      return false;
+    }
+    const value = this.queue[this.head];
+    this.head = (this.head + 1) % this.length;
+    return value;
+  }
+}
