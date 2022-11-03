9af572ded84bb6c993da2e0614523c2129a343c6
yi.shi <kukudeshiyi@yeah.net>
2022-11-03 13:33:16 +0800

    feat: 调试泛型算法

--- cpp/gettingStarted/03/04_generic_algorithm.cpp
+++ cpp/gettingStarted/03/04_generic_algorithm.cpp
@@ -3,11 +3,18 @@
 #include<algorithm>
 using namespace std;
 
-extern bool grow_vec( vector<int>&, int );
+template<typename type>
+bool grow_vec( vector<type> &vec, type val ) {
+    if(vec.size() < 2)
+        vec.insert(vec.begin(), 2 - vec.size(), 1);
+    while(vec[vec.size()-1] < val)
+        vec.push_back(vec[vec.size()-1] + vec[vec.size() - 2]);
+    return vec[vec.size() - 1] == val;
+}
 
 bool is_elem( vector<int> &vec, int elem ){
-  // int max_value = max_element( vec.begin(), vec.end() );
-  int max_value = vec.empty() ? 0 : vec[vec.size()-1];
+  int max_value = *max_element( vec.begin(), vec.end() );
+  // int max_value = vec.empty() ? 0 : vec[vec.size()-1];
   if( max_value < elem){
     return grow_vec( vec, elem );
   }
@@ -18,5 +25,11 @@ bool is_elem( vector<int> &vec, int elem ){
 }
 
 int main(){
+  int arr[8] = {
+    1,2,3,4,
+    5,6,7,8
+  };
+  vector<int> vec( arr, arr+8 );
+  cout << "result is " << (is_elem( vec, 8 ) ? "true" : "false") << endl;
   return 0;
 }
