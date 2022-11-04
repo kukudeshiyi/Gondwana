2b3a25b9ba2be74effb8516e937ae9cbe8ba9f8e
yi.shi <kukudeshiyi@yeah.net>
2022-11-04 13:38:47 +0800

    feat: add cases

--- cpp/gettingStarted/03/05_design_generic_algorithm.cpp
+++ cpp/gettingStarted/03/05_design_generic_algorithm.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<functional>
+#include<algorithm>
 using namespace std;
 
 vector<int> less_than_10( const vector<int> &vec ){
@@ -23,7 +25,9 @@ vector<int> filter( const vector<int> &vec, int filterValue, bool (*pred)( int, 
   return nvec;
 }
 
-// function object
+// 上面实现的 filter，传入的是函数指针来自定义，所以我们使用 function object
+// 另外可以使用 function object adapter 可以给 function object 绑定一个参数，这样用户只需要传递另外一个参数就可以了。
+
 
 int main(){
   const int size = 8;
@@ -34,5 +38,15 @@ int main(){
   vector<int> vec( arr, arr+size );
   vector<int> vec_1 = less_than_10(vec);
   cout << "result is " << vec_1[0] << ' ' << vec_1[ vec_1.size() - 1 ] << endl;
+
+
+  // sort 排序默认是升序排列，可以传递 function object 让 sort 以降序排列
+  sort( vec.begin(), vec.end(), greater<int>() );
+  cout << "sort result is " << vec[0] << ' ' << vec[ vec.size() - 1 ] << endl;
+  cout << "binary search result is " << binary_search( vec.begin(), vec.end(), 4, greater<int>() ) << endl;
+
+
+  // function object adapter
+
   return 0;
 }
