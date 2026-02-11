4e7b7977d90bf17f56699b414dd26ffc0d14c58f
liqi.shi <liqi.shi@shopee.com>
2026-02-11 15:53:41 +0800

    daily commit

--- frontend/javascript/20_generator.js
+++ frontend/javascript/20_generator.js
@@ -21,3 +21,40 @@
 // console.log("1", a); // -> '1' 1
 
 // 模拟一个自动执行函数
+
+function myFetch(sec) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(sec);
+    }, sec);
+  });
+}
+
+function* fetchBusinessData() {
+  const value1 = yield myFetch(1000);
+  console.log("1");
+  const value2 = yield myFetch(3000);
+  console.log("2");
+  const value3 = yield myFetch(4000);
+  console.log("values:", value1, value2, value3);
+}
+
+function autoFn(fn) {
+  const iterator = fn();
+
+  function step(val) {
+    const result = iterator.next(val);
+
+    if (result.done) {
+      return result.value;
+    }
+
+    result.value.then((data) => {
+      step(data);
+    });
+  }
+
+  return step();
+}
+
+autoFn(fetchBusinessData);
